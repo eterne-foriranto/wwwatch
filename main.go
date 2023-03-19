031257ef63650485cd90b92cdaf1a991680b883c
Yevgeni Starostin <nido-svj@mail.ru>
2023-03-20 01:10:38 +0300

    feat: get basic info about group

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
-package wwwatch
+package main
 
 import (
+	"fmt"
 	"github.com/SevereCloud/vksdk/api"
 	"os"
 )
@@ -8,4 +9,11 @@ import (
 func main() {
 	token := os.Getenv("USER_TOKEN")
 	vk := api.NewVK(token)
+	params := api.Params{"group_id": "chgk_ivanovo"}
+	group, err := vk.GroupsGetByID(params)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(group)
 }
