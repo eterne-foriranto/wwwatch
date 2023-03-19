faad016bcde574b2b34b3c3a59a4a6c44b0a10d6
Yevgeni Starostin <nido-svj@mail.ru>
2023-03-20 02:30:05 +0300

    feat: get comments

--- main.go
+++ main.go
@@ -10,10 +10,29 @@ func main() {
 	token := os.Getenv("USER_TOKEN")
 	vk := api.NewVK(token)
 	params := api.Params{"group_id": "chgk_ivanovo"}
-	group, err := vk.GroupsGetByID(params)
+	groups, err := vk.GroupsGetByID(params)
 
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(group)
+
+	groupId := groups[0].ID
+	params["group_id"] = groupId
+	topics, err := vk.BoardGetTopics(params)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	topicId := topics.Items[0].ID
+	params["topic_id"] = topicId
+	comments, err := vk.BoardGetComments(params)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, comment := range comments.Items {
+		fmt.Println(comment)
+	}
 }
