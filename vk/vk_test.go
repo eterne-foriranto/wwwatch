501d1109588515a75afc77fa2bb0f7fff96f42c9
Yevgeni Starostin <nido-svj@mail.ru>
2023-03-24 08:01:40 +0300

    feat: improve test

--- vk/vk_test.go
+++ vk/vk_test.go
@@ -1,14 +1,20 @@
 package vk
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
+
+const FileName = "topic_id.dat"
 
 func TestCheckComment(t *testing.T) {
+	os.Remove(FileName)
 	teamName := "Койнфлип"
-	want := "Койнфлип"
-	comment, status := CheckComment(3, "topic_id.dat", teamName)
+	want := "https://vk.com/topic-173798358_49268096?post=2245"
+	comment, status := CheckComment(3, FileName, teamName)
 
 	if comment != want || status != true {
-		t.Fatalf(`CheckComment(3, "topic_id.dat", %q) = %q, %v, 
-want match for %q, true`, teamName, comment, status, want)
+		t.Fatalf(`CheckComment(3, %v, %q) = %q, %v, 
+want match for %q, true`, FileName, teamName, comment, status, want)
 	}
 }
