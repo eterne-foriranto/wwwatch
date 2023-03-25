74d97899cc7a60215fae69c8141aaf278e1c6ef4
Yevgeni Starostin <nido-svj@mail.ru>
2023-03-25 04:57:00 +0300

    refactor: move vk functions to main package

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
-import "eterne-foriranto/wwwatch/vk"
-
 const TeamName = "Охтыжёжик"
 const Cutoff = 3
 const FileName = "topic_id.dat"
 
 func main() {
-	url, status := vk.CheckComment(Cutoff, FileName, TeamName)
+	url, status := checkComment(Cutoff, FileName, TeamName)
 
 	if status {
 		send(url)
---
+++ vk_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+const TestFileName = "topic_id.dat"
+
+func TestCheckComment(t *testing.T) {
+	os.Remove(TestFileName)
+	teamName := "Койнфлип"
+	want := "https://vk.com/topic-173798358_49268096?post=2245"
+	comment, status := checkComment(3, TestFileName, teamName)
+
+	if comment != want || status != true {
+		t.Fatalf(`checkComment(3, %v, %q) = %q, %v, 
+want match for %q, true`, TestFileName, teamName, comment, status, want)
+	}
+}
